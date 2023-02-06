68d0391ca9fce54c2e2dca6f2b10ab104d05a043
mponeoth <58957165+mponeoth@users.noreply.github.com>
2023-02-06 00:41:15 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//set initial count
+let count = 0
+
+// select value and buttons 
+
+    let value = document.querySelector('#value')
+    let btns = document.querySelectorAll('.btn')
+
+    btns.forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            const styles =e.currentTarget.classList
+            if(styles.contains('increase')){
+                count++;
+            }else if(styles.contains('decrease')){
+                count--;
+            }else{
+                count = 0
+            }
+             if(count>0){
+                value.style.color ='green'
+             }
+             else if(count<0){
+                value.style.color ='red'
+             }
+             else{
+                value.style.color ='black'
+             }
+
+
+            value.textContent = count
+        })
+    })
+
+
